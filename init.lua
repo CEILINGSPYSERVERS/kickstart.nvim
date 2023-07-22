cae0a4517456dd6afe3b1f59b2f533528bb3485f
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2023-07-21 22:06:17 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -502,8 +502,8 @@ vim.opt.nu = true
 -- Other setups from ThePrimeagen
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+-- vim.opt.shiftwidth = 4
+-- vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
