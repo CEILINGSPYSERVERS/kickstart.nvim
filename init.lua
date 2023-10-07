fc5752359d5054f158696eaa1bf8fdb5c00bb69f
Logan-Wessel <lgwessel@iastate.edu>
2023-10-07 12:46:01 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -278,7 +278,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'javascript' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'rust', 'vimdoc', 'vim', 'javascript' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = true,
@@ -499,7 +499,8 @@ cmp.setup {
 }
 
 -- Enable number lines
-vim.opt.nu = true
+vim.opt.rnu = true
+
 
 -- Other setups from ThePrimeagen
 vim.opt.tabstop = 4
